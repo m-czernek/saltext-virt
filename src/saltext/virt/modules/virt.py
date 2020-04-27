saltstack/salt

Add bhyve compatibility to virt

salt/modules/virt.py
@@ -180,6 +180,7 @@ def __get_conn(**kwargs):
     '''
     # This has only been tested on kvm and xen, it needs to be expanded to
     # support all vm layers supported by libvirt
+    # Connection string works on bhyve, but auth is not tested.
 
     username = kwargs.get('username', None)
     password = kwargs.get('password', None)
@@ -467,6 +468,11 @@ def _get_disks(dom):
             qemu_target = source.get('file', '')
             if not qemu_target:
                 qemu_target = source.get('dev', '')
+            elif qemu_target.startswith('/dev/zvol/'):
+                disks[target.get('dev')] = {
+                        'file': qemu_target,
+                        'zfs': True}
+                continue
             if not qemu_target and 'protocol' in source.attrib and 'name' in source.attrib:  # for rbd network
                 qemu_target = '{0}:{1}'.format(
                         source.get('protocol'),
@@ -619,7 +625,7 @@ def _gen_xml(name,
         context['disks'][disk['name']] = {}
         context['disks'][disk['name']]['file_name'] = disk['filename']
         context['disks'][disk['name']]['source_file'] = disk['source_file']
-        if hypervisor in ['qemu', 'kvm']:
+        if hypervisor in ['qemu', 'kvm', 'bhyve']:
             context['disks'][disk['name']]['target_dev'] = 'vd{0}'.format(string.ascii_lowercase[i])
             context['disks'][disk['name']]['address'] = False
             context['disks'][disk['name']]['driver'] = True
@@ -630,7 +636,6 @@ def _gen_xml(name,
         context['disks'][disk['name']]['disk_bus'] = disk['model']
         context['disks'][disk['name']]['type'] = disk['format']
         context['disks'][disk['name']]['index'] = six.text_type(i)
-
     context['nics'] = nicp
 
     fn_ = 'libvirt_domain.jinja'
@@ -749,6 +754,76 @@ def _get_images_dir():
     log.debug('Image directory from config option `virt:images`'
               ' is %s', img_dir)
     return img_dir
+
+
+def _zfs_image_create(vm_name,
+                      pool,
+                      disk_name,
+                      hostname_property_name,
+                      sparse_volume,
+                      disk_size,
+                      disk_image_name):
+    '''
+    Clones an existing image, or creates a new one.
+
+    When cloning an image, disk_image_name refers to the source
+    of the clone. If not specified, disk_size is used for creating
+    a new zvol, and sparse_volume determines whether to create
+    a thin provisioned volume.
+
+    The cloned or new volume can have a ZFS property set containing
+    the vm_name. Use hostname_property_name for specifying the key
+    of this ZFS property.
+    '''
+    if not disk_image_name and not disk_size:
+        raise CommandExecutionError(
+            'Unable to create new disk {0}, please specify'
+            ' the disk image name or disk size argument'
+            .format(disk_name)
+        )
+
+    if not pool:
+        raise CommandExecutionError(
+            'Unable to create new disk {0}, please specify'
+            ' the disk pool name'.format(disk_name))
+
+    destination_fs = os.path.join(pool,
+                                  '{0}.{1}'.format(vm_name, disk_name))
+    log.debug('Image destination will be %s', destination_fs)
+
+    existing_disk = __salt__['zfs.list'](name=pool)
+    if 'error' in existing_disk:
+        raise CommandExecutionError(
+            'Unable to create new disk {0}. {1}'
+            .format(destination_fs, existing_disk['error'])
+        )
+    elif destination_fs in existing_disk:
+        log.info('ZFS filesystem {0} already exists. Skipping creation'
+                 .format(destination_fs))
+        blockdevice_path = os.path.join('/dev/zvol', pool, vm_name)
+        return blockdevice_path
+
+    properties = {}
+    if hostname_property_name:
+        properties[hostname_property_name] = vm_name
+
+    if disk_image_name:
+        __salt__['zfs.clone'](
+                  name_a=disk_image_name,
+                  name_b=destination_fs,
+                  properties=properties)
+
+    elif disk_size:
+        __salt__['zfs.create'](
+                name=destination_fs,
+                properties=properties,
+                volume_size=disk_size,
+                sparse=sparse_volume)
+
+    blockdevice_path = os.path.join('/dev/zvol', pool, '{0}.{1}'
+                                    .format(vm_name, disk_name))
+    log.debug('Image path will be %s', blockdevice_path)
+    return blockdevice_path
 
 
 def _qemu_image_create(disk, create_overlay=False, saltenv='base'):
@@ -893,6 +968,10 @@ def _disk_profile(profile, hypervisor, disks=None, vm_name=None, image=None, poo
     elif hypervisor in ['qemu', 'kvm']:
         overlay = {'format': 'qcow2',
                    'model': 'virtio'}
+    elif hypervisor in ['bhyve']:
+        overlay = {'format': 'raw',
+                   'model': 'virtio',
+                   'sparse_volume': False}
     else:
         overlay = {}
 
@@ -943,9 +1022,11 @@ def _disk_profile(profile, hypervisor, disks=None, vm_name=None, image=None, poo
                                     'Unable to create new disk {0}, specified pool {1} does not exist '
                                     'or is unsupported'.format(disk['name'], base_dir))
                     base_dir = pool['target_path']
-
-        # Compute the filename and source file properties if possible
-        if vm_name:
+        if hypervisor == 'bhyve' and vm_name:
+            disk['filename'] = '{0}.{1}'.format(vm_name, disk['name'])
+            disk['source_file'] = os.path.join('/dev/zvol', base_dir or '', disk['filename'])
+        elif vm_name:
+            # Compute the filename and source file properties if possible
             disk['filename'] = '{0}_{1}.{2}'.format(vm_name, disk['name'], disk['format'])
             disk['source_file'] = os.path.join(base_dir, disk['filename'])
 
@@ -959,10 +1040,12 @@ def _complete_nics(interfaces, hypervisor, dmac=None):
 
     vmware_overlay = {'type': 'bridge', 'source': 'DEFAULT', 'model': 'e1000'}
     kvm_overlay = {'type': 'bridge', 'source': 'br0', 'model': 'virtio'}
+    bhyve_overlay = {'type': 'bridge', 'source': 'bridge0', 'model': 'virtio'}
     overlays = {
             'kvm': kvm_overlay,
             'qemu': kvm_overlay,
             'vmware': vmware_overlay,
+            'bhyve': bhyve_overlay,
             }
 
     def _normalize_net_types(attributes):
@@ -1315,6 +1398,32 @@ def init(name,
         ``True`` to create a QCOW2 disk image with ``image`` as backing file. If ``False``
         the file pointed to by the ``image`` property will simply be copied. (Default: ``False``)
 
+    hostname_property
+        When using ZFS volumes, setting this value to a ZFS property ID will make Salt store the name of the
+        virtual machine inside this property. (Default: ``None``)
+
+    sparse_volume
+        Boolean to specify whether to use a thin provisioned ZFS volume.
+
+        Example profile for a bhyve VM with two ZFS disks. The first is
+        cloned from the specified image. The second disk is a thin
+        provisioned volume.
+
+        .. code-block:: yaml
+
+            virt:
+              disk:
+                two_zvols:
+                  - system:
+                      image: zroot/bhyve/CentOS-7-x86_64-v1@v1.0.5
+                      hostname_property: virt:hostname
+                      pool: zroot/bhyve/guests
+                  - data:
+                      pool: tank/disks
+                      size: 20G
+                      hostname_property: virt:hostname
+                      sparse_volume: True
+
     .. _init-graphics-def:
 
     .. rubric:: Graphics Definition
@@ -1472,6 +1581,17 @@ def init(name,
                     pub_key=pub_key,
                     priv_key=priv_key,
                 )
+
+        elif hypervisor in ['bhyve']:
+            img_dest = _zfs_image_create(
+                vm_name=name,
+                pool=_disk.get('pool'),
+                disk_name=_disk.get('name'),
+                disk_size=_disk.get('size'),
+                disk_image_name=_disk.get('image'),
+                hostname_property_name=_disk.get('hostname_property'),
+                sparse_volume=_disk.get('sparse_volume'),
+            )
 
         else:
             # Unknown hypervisor
@@ -3156,8 +3276,17 @@ def purge(vm_, dirs=False, removables=None, **kwargs):
     for disk in disks:
         if not removables and disks[disk]['type'] in ['cdrom', 'floppy']:
             continue
-        os.remove(disks[disk]['file'])
-        directories.add(os.path.dirname(disks[disk]['file']))
+        elif disks[disk].get('zfs', False):
+            # TODO create solution for 'dataset is busy'
+            time.sleep(3)
+            fs_name = disks[disk]['file'][len('/dev/zvol/'):]
+            log.info('Destroying VM ZFS volume {0}'.format(fs_name))
+            __salt__['zfs.destroy'](
+                    name=fs_name,
+                    force=True)
+        else:
+            os.remove(disks[disk]['file'])
+            directories.add(os.path.dirname(disks[disk]['file']))
     if dirs:
         for dir_ in directories:
             shutil.rmtree(dir_)
@@ -3265,6 +3394,7 @@ def get_hypervisor():
 
     - kvm
     - xen
+    - bhyve
 
     CLI Example:
 
@@ -3273,13 +3403,26 @@ def get_hypervisor():
         salt '*' virt.get_hypervisor
 
     .. versionadded:: Fluorine
-        the function and the ``kvm`` and ``xen`` hypervisors support
+        the function and the ``kvm``, ``xen`` and ``bhyve`` hypervisors support
     '''
     # To add a new 'foo' hypervisor, add the _is_foo_hyper function,
     # add 'foo' to the list below and add it to the docstring with a .. versionadded::
-    hypervisors = ['kvm', 'xen']
-    result = [hyper for hyper in hypervisors if getattr(sys.modules[__name__], '_is_{}_hyper').format(hyper)()]
+    hypervisors = ['kvm', 'xen', 'bhyve']
+    result = [hyper for hyper in hypervisors if getattr(sys.modules[__name__], '_is_{}_hyper'.format(hyper))()]
     return result[0] if result else None
+
+
+def _is_bhyve_hyper():
+    sysctl_cmd = 'sysctl hw.vmm.create'
+    vmm_enabled = False
+    try:
+        stdout = subprocess.Popen(sysctl_cmd,
+                                  shell=True,
+                                  stdout=subprocess.PIPE).communicate()[0]
+        vmm_enabled = len(salt.utils.stringutils.to_str(stdout).split('"')[1]) != 0
+    except IndexError:
+        pass
+    return vmm_enabled
 
 
 def is_hyper():
@@ -3293,7 +3436,7 @@ def is_hyper():
         salt '*' virt.is_hyper
     '''
     if HAS_LIBVIRT:
-        return is_xen_hyper() or is_kvm_hyper()
+        return is_xen_hyper() or is_kvm_hyper() or _is_bhyve_hyper()
     return False
 
 
@@ -3811,8 +3954,7 @@ def _parse_caps_guest(guest):
     # without possibility to toggle them.
     result['features'] = {child.tag: {'toggle': True if child.get('toggle') == 'yes' else False,
                                       'default': True if child.get('default') == 'no' else True}
-                          for child in guest.find('features')}
-
+                          for child in guest.find('features') or []}
     return result
 
 

m-czernek/saltext-virt

src/saltext/virt/modules/virt.py
@@ -205,6 +205,7 @@ def __get_conn(**kwargs):
     """
     # This has only been tested on kvm and xen, it needs to be expanded to
     # support all vm layers supported by libvirt
+    # Connection string works on bhyve, but auth is not tested.
 
     username = kwargs.get("username", None)
     password = kwargs.get("password", None)
@@ -476,6 +477,9 @@ def _get_disks(dom):
                 qemu_target = "{0}:{1}".format(
                     source.get("protocol"), source.get("name")
                 )
+            elif qemu_target.startswith("/dev/zvol/"):
+                disks[target.get("dev")] = {"file": qemu_target, "zfs": True}
+                continue
             if not qemu_target:
                 continue
 
@@ -800,6 +804,82 @@ def _get_images_dir():
     return img_dir
 
 
+def _zfs_image_create(
+    vm_name,
+    pool,
+    disk_name,
+    hostname_property_name,
+    sparse_volume,
+    disk_size,
+    disk_image_name,
+):
+    """
+    Clones an existing image, or creates a new one.
+
+    When cloning an image, disk_image_name refers to the source
+    of the clone. If not specified, disk_size is used for creating
+    a new zvol, and sparse_volume determines whether to create
+    a thin provisioned volume.
+
+    The cloned or new volume can have a ZFS property set containing
+    the vm_name. Use hostname_property_name for specifying the key
+    of this ZFS property.
+    """
+    if not disk_image_name and not disk_size:
+        raise CommandExecutionError(
+            "Unable to create new disk {0}, please specify"
+            " the disk image name or disk size argument".format(disk_name)
+        )
+
+    if not pool:
+        raise CommandExecutionError(
+            "Unable to create new disk {0}, please specify"
+            " the disk pool name".format(disk_name)
+        )
+
+    destination_fs = os.path.join(pool, "{0}.{1}".format(vm_name, disk_name))
+    log.debug("Image destination will be %s", destination_fs)
+
+    existing_disk = __salt__["zfs.list"](name=pool)
+    if "error" in existing_disk:
+        raise CommandExecutionError(
+            "Unable to create new disk {0}. {1}".format(
+                destination_fs, existing_disk["error"]
+            )
+        )
+    elif destination_fs in existing_disk:
+        log.info(
+            "ZFS filesystem {0} already exists. Skipping creation".format(
+                destination_fs
+            )
+        )
+        blockdevice_path = os.path.join("/dev/zvol", pool, vm_name)
+        return blockdevice_path
+
+    properties = {}
+    if hostname_property_name:
+        properties[hostname_property_name] = vm_name
+
+    if disk_image_name:
+        __salt__["zfs.clone"](
+            name_a=disk_image_name, name_b=destination_fs, properties=properties
+        )
+
+    elif disk_size:
+        __salt__["zfs.create"](
+            name=destination_fs,
+            properties=properties,
+            volume_size=disk_size,
+            sparse=sparse_volume,
+        )
+
+    blockdevice_path = os.path.join(
+        "/dev/zvol", pool, "{0}.{1}".format(vm_name, disk_name)
+    )
+    log.debug("Image path will be %s", blockdevice_path)
+    return blockdevice_path
+
+
 def _qemu_image_create(disk, create_overlay=False, saltenv="base"):
     """
     Create the image file using specified disk_size or/and disk_image
@@ -941,6 +1021,8 @@ def _disk_profile(profile, hypervisor, disks=None, vm_name=None, **kwargs):
         overlay = {"format": "qcow2", "device": "disk", "model": "virtio"}
     elif hypervisor == "xen":
         overlay = {"format": "qcow2", "device": "disk", "model": "xen"}
+    elif hypervisor in ["bhyve"]:
+        overlay = {"format": "raw", "model": "virtio", "sparse_volume": False}
     else:
         overlay = {}
 
@@ -1006,8 +1088,13 @@ def _fill_disk_filename(vm_name, disk, hypervisor, **kwargs):
                     )
                 base_dir = pool["target_path"]
 
-    # Compute the filename and source file properties if possible
-    if vm_name:
+    if hypervisor == "bhyve" and vm_name:
+        disk["filename"] = "{0}.{1}".format(vm_name, disk["name"])
+        disk["source_file"] = os.path.join(
+            "/dev/zvol", base_dir or "", disk["filename"]
+        )
+    elif vm_name:
+        # Compute the filename and source file properties if possible
         disk["filename"] = "{0}_{1}.{2}".format(vm_name, disk["name"], disk["format"])
         disk["source_file"] = os.path.join(base_dir, disk["filename"])
 
@@ -1020,11 +1107,13 @@ def _complete_nics(interfaces, hypervisor):
     vmware_overlay = {"type": "bridge", "source": "DEFAULT", "model": "e1000"}
     kvm_overlay = {"type": "bridge", "source": "br0", "model": "virtio"}
     xen_overlay = {"type": "bridge", "source": "br0", "model": None}
+    bhyve_overlay = {"type": "bridge", "source": "bridge0", "model": "virtio"}
     overlays = {
         "xen": xen_overlay,
         "kvm": kvm_overlay,
         "qemu": kvm_overlay,
         "vmware": vmware_overlay,
+        "bhyve": bhyve_overlay,
     }
 
     def _normalize_net_types(attributes):
@@ -1355,6 +1444,32 @@ def init(
         Type of device of the disk. Can be one of 'disk', 'cdrom', 'floppy' or 'lun'.
         (Default: ``'disk'``)
 
+    hostname_property
+        When using ZFS volumes, setting this value to a ZFS property ID will make Salt store the name of the
+        virtual machine inside this property. (Default: ``None``)
+
+    sparse_volume
+        Boolean to specify whether to use a thin provisioned ZFS volume.
+
+        Example profile for a bhyve VM with two ZFS disks. The first is
+        cloned from the specified image. The second disk is a thin
+        provisioned volume.
+
+        .. code-block:: yaml
+
+            virt:
+              disk:
+                two_zvols:
+                  - system:
+                      image: zroot/bhyve/CentOS-7-x86_64-v1@v1.0.5
+                      hostname_property: virt:hostname
+                      pool: zroot/bhyve/guests
+                  - data:
+                      pool: tank/disks
+                      size: 20G
+                      hostname_property: virt:hostname
+                      sparse_volume: True
+
     .. _init-graphics-def:
 
     .. rubric:: Graphics Definition
@@ -1470,6 +1585,17 @@ def init(
                     pub_key=pub_key,
                     priv_key=priv_key,
                 )
+
+        elif hypervisor in ["bhyve"]:
+            img_dest = _zfs_image_create(
+                vm_name=name,
+                pool=_disk.get("pool"),
+                disk_name=_disk.get("name"),
+                disk_size=_disk.get("size"),
+                disk_image_name=_disk.get("image"),
+                hostname_property_name=_disk.get("hostname_property"),
+                sparse_volume=_disk.get("sparse_volume"),
+            )
 
         else:
             # Unknown hypervisor
@@ -3356,8 +3482,15 @@ def purge(vm_, dirs=False, removables=False, **kwargs):
     for disk in disks:
         if not removables and disks[disk]["type"] in ["cdrom", "floppy"]:
             continue
-        os.remove(disks[disk]["file"])
-        directories.add(os.path.dirname(disks[disk]["file"]))
+        elif disks[disk].get("zfs", False):
+            # TODO create solution for 'dataset is busy'
+            time.sleep(3)
+            fs_name = disks[disk]["file"][len("/dev/zvol/") :]
+            log.info("Destroying VM ZFS volume {0}".format(fs_name))
+            __salt__["zfs.destroy"](name=fs_name, force=True)
+        else:
+            os.remove(disks[disk]["file"])
+            directories.add(os.path.dirname(disks[disk]["file"]))
     if dirs:
         for dir_ in directories:
             shutil.rmtree(dir_)
@@ -3428,6 +3561,7 @@ def get_hypervisor():
 
     - kvm
     - xen
+    - bhyve
 
     CLI Example:
 
@@ -3436,17 +3570,30 @@ def get_hypervisor():
         salt '*' virt.get_hypervisor
 
     .. versionadded:: 2019.2.0
-        the function and the ``kvm`` and ``xen`` hypervisors support
+        the function and the ``kvm``, ``xen`` and ``bhyve`` hypervisors support
     """
     # To add a new 'foo' hypervisor, add the _is_foo_hyper function,
     # add 'foo' to the list below and add it to the docstring with a .. versionadded::
-    hypervisors = ["kvm", "xen"]
+    hypervisors = ["kvm", "xen", "bhyve"]
     result = [
         hyper
         for hyper in hypervisors
         if getattr(sys.modules[__name__], "_is_{}_hyper".format(hyper))()
     ]
     return result[0] if result else None
+
+
+def _is_bhyve_hyper():
+    sysctl_cmd = "sysctl hw.vmm.create"
+    vmm_enabled = False
+    try:
+        stdout = subprocess.Popen(
+            sysctl_cmd, shell=True, stdout=subprocess.PIPE
+        ).communicate()[0]
+        vmm_enabled = len(salt.utils.stringutils.to_str(stdout).split('"')[1]) != 0
+    except IndexError:
+        pass
+    return vmm_enabled
 
 
 def is_hyper():
@@ -3460,7 +3607,7 @@ def is_hyper():
         salt '*' virt.is_hyper
     """
     if HAS_LIBVIRT:
-        return _is_xen_hyper() or _is_kvm_hyper()
+        return _is_xen_hyper() or _is_kvm_hyper() or _is_bhyve_hyper()
     return False
 
 